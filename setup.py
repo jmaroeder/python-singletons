66c76508b70f9db40b6df245d5370450709e6bd2
James M. Allen <james.m.allen@gmail.com>
2018-01-23 14:18:42 -0500

    updating pypi description

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     name='singletons',
     version='0.2.0',
     license='MIT license',
-    description='An example package. Generated with cookiecutter-pylibrary.',
+    description='Singleton metaclasses and singleton factories',
     long_description='%s\n%s' % (
         re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.rst')),
         re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst'))
