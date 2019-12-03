86265e3f35811e288ef8ee8d365850b8bd1762bd
James Roeder <jmaroeder@gmail.com>
2019-12-03 15:05:37 -0500

    Bump version: 0.2.3 → 0.2.4

--- src/singletons/__init__.py
+++ src/singletons/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '__version__ = '0.2.3''
+__version__ = '__version__ = '0.2.4''
 
 from .factory import EventletFactory, GeventFactory, GlobalFactory, GreenthreadFactory, ProcessFactory, ThreadFactory
 from .singleton import EventletSingleton, GeventSingleton, GreenthreadSingleton, ProcessSingleton, Singleton, \
